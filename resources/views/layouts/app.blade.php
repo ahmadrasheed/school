c49c1eb1e6d77d43ddf4fa2cb4a36bfe3f1a6700
Ahmad-Mac <ahmad.rasheed1@yahoo.com>
2019-09-21 22:19:07 +0300

    carosel added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,7 @@
                 </div>
                 <div class="row mt-2">
                   <div class="col-sm-12 d-flex justify-content-center">
-                      <p class="myfont"> العراق - الموصل - المجموعة الثقافية - ملتقى الكتاب</p>
+                      <p class="myfont">العراق - مدينة الموصل - حي الشرطة </p>
                   </div>
                   
                 </div>
